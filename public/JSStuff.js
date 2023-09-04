22e6103401eeaea5e98ce56b16302e29055415b3
lule345 <124836785+lule345@users.noreply.github.com>
2023-09-04 09:57:01 -0400

    Update JSStuff.js

--- public/JSStuff.js
+++ public/JSStuff.js
@@ -1,14 +1,2 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+var audio = new Audio("https://drive.google.com/uc?export=download&id=1qaxXIzQNogXUeqOwThKZ86nImMvro_zW")
+audio.loop = true
