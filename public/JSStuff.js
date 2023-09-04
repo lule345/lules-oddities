395b9f3464433c1de5f009bf55f73720093b625d
lule345 <124836785+lule345@users.noreply.github.com>
2023-09-04 10:06:25 -0400

    Update JSStuff.js

--- public/JSStuff.js
+++ public/JSStuff.js
@@ -1,2 +1,6 @@
-var audio = new Audio("https://drive.google.com/uc?export=download&id=1qaxXIzQNogXUeqOwThKZ86nImMvro_zW")
+var music = new Audio("https://drive.google.com/uc?export=download&id=1qaxXIzQNogXUeqOwThKZ86nImMvro_zW")
 audio.loop = true
+
+function audioplay() {
+  music.play();
+}
